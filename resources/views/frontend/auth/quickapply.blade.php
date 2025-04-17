547b50fa4128f7b17954567a85ffe70c6999ff35
awal <awal2645>
2025-04-17 18:46:16 +0600

    design improve cv submit page

--- resources/views/frontend/auth/quickapply.blade.php
+++ resources/views/frontend/auth/quickapply.blade.php
@@ -71,7 +71,7 @@
 
                                 <div class="col-lg-6">
                                     <div class="row">
-                                        <div class="col-6">
+                                        <div class=" col-12">
                                             <input type="tel" name="prefixphone" required id="phone" 
                                                 class="form-control @error('prefixphone') is-invalid @enderror" 
                                                 value="{{ old('prefixphone') }}">
